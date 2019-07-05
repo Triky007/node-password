1152ae21cf94bdf3a1ae1ef65191a0ed00b3b1fb
Triky007 <triky007@hotmail.com>
2019-07-05 09:16:37 +0200

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//'url': 'mongodb://localhost/nodelogin'
-	'url' :'mongodb://ds347367.mlab.com:47367/heroku_8vmj0wbf'
+	'url' :'mongodb://triky007:triky.123@ds347367.mlab.com:47367/heroku_8vmj0wbf'
 };
